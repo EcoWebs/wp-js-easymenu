7a852504231c3404a56baed38b2467897c6728da
EcoWebs <38787815+EcoWebs@users.noreply.github.com>
2018-04-27 14:28:15 +0100

    Create wp-js-easymenu.js

---
+++ wp-js-easymenu.js
@@ -0,0 +1,38 @@
+function getEasyMenuStatus() {
+  var status = $.cookie('wp_easy_menu_status');
+  if ([undefined, null, false, "undefined", "null", "false"].indexOf(status) >= 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function toggleEasyMenuStatus() {
+  $.cookie('wp_easy_menu_status', !getEasyMenuStatus());
+  if (getEasyMenuStatus()) {
+    $('body').addClass('easy_menu_off');
+  } else {
+    $('body').removeClass('easy_menu_off');
+  }
+}
+
+
+$(function () {
+  
+  
+  $('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/EcoWebs/wp-js-easymenu/wp-js-easymenu.css">');
+
+
+  var t_menu = $('#wp-admin-bar-root-default');
+  var easy_menuToggle = $('<li id="easy-toggle-menu" class="menupop"><a class="ab-item" href="#">Cambiar Modo</a></li>').appendTo(t_menu);
+
+  easy_menuToggle.on('click', function () {
+    toggleEasyMenuStatus();
+  });
+  
+  if (getEasyMenuStatus()) {
+    $('body').addClass('easy_menu_off');
+  } else {
+    $('body').removeClass('easy_menu_off');
+  }
+});
